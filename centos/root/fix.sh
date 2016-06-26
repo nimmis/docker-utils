2c46b0197b2bfa3299573bd03c5abb8242153e16
nimmis <kjell.havneskold@gmail.com>
2016-06-26 10:18:23 +0200

    fix differences between versions i CentOS

--- centos/root/fix.sh
+++ centos/root/fix.sh
@@ -1,13 +1,53 @@
 #!/bin/bash
 
-# set US locale UTF-8
-localedef -c -i en_US -f UTF-8 en_US.UTF-8
+# install script for centos 5-7
+#
+# (c) nimmis <kjell.havneskold@gmail.com>, 2016
+#
 
-# enable EPEL repository
-yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+
+# fix missing dir
+mkdir -p /usr/share/info/
 
 # install some often used commands
 yum install -y wget curl zip unzip patch less git vim nano psmisc
+
+# do versiondependent installations
+
+RELEASEVER=$(rpm -q --qf "%{VERSION}" $(rpm -q --whatprovides redhat-release))
+
+case "$RELEASEVER" in
+  5)
+	# enable EPEL repositor
+	wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-5.noarch.rpm
+	rpm -Uhv epel-release-latest-5.noarch.rpm
+	rm epel-release-latest-5.noarch.rpm
+
+	# install crontab
+	yum install -y vixie-cron
+
+	;;
+  6)
+	# enable EPEL repository
+	yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
+
+	# install crontab
+	yum install -y cronie
+
+	;;
+  7)
+	# enable EPEL repository
+	yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+
+	# install crontab
+	yum install -y cronie
+
+	;;
+esac
+
+# set US locale UTF-8
+localedef -c -i en_US -f UTF-8 en_US.UTF-8
+
 
 # installing syslog-ng, with workaround https://bugs.launchpad.net/ubuntu/+source/syslog-ng/+bug/1242173
 yum install -y syslog-ng
@@ -18,7 +58,7 @@ sed -i -E 's/^(\s*)system\(\);/\1unix-stream("\/dev\/log");/' /etc/syslog-ng/sys
 yum install -y supervisor
 
 # setup cron and logrotatae
-yum install -y cronie logrotate
+yum install -y logrotate
 
 
 mkdir /etc/container_environment
